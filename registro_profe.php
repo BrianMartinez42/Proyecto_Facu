844570dcfb6b90b74f33949131a910f012a267bb
marbrian <martinezbrian.exe@gmail.com>
2021-11-22 18:06:11 -0300

    Arreglo registro de profesor

--- registro_profe.php
+++ registro_profe.php
@@ -7,14 +7,17 @@
   </head>
 
   <body>
+    <header>
+      <h1 class="contenedor__title">Infinexus</h1>
+    </header>
     <?php
       //incluir el archivo que conecta con la base de datos
       include_once("conexion.php");
 
       if(isset($_POST['Enviar'])) {
-      	$nombre = mysqli_real_escape_string($mysqli, $_POST['nombre']);
-      	$mail = mysqli_real_escape_string($mysqli, $_POST['mail']);
-        $pass = mysqli_real_escape_string($mysqli, $_POST['pass']);
+      	$nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
+      	$mail = mysqli_real_escape_string($conexion, $_POST['mail']);
+        $pass = mysqli_real_escape_string($conexion, $_POST['pass']);
 
       	// verificar campos vacios
       	if(empty($nombre) || empty($mail)) {
@@ -40,10 +43,17 @@
       		$result = mysqli_query($conexion, "INSERT INTO profe(nombre,mail,pass) VALUES('$nombre','$mail','$pass')");
 
       		//mostrar mensaje de exito
-      		echo "<font color='green'>Datos agregados correctamente.";
-      		echo "<br/><a class='button-grey' href='login_profe.html'>Iniciar Sesión</a>";
+          ?>
+          <div class='mensaje-b'>
+          <h3>Se ha registrado correctamente.</h3><br>
+            <div class="form-group">
+              <a class='button-grey' href='login_profe.html'>Iniciar Sesión</a>
+            </div>
+          </div>
+          <?php
       	}
       }
+
     ?>
   </body>
 </html>
